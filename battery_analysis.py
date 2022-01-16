4740b92cc160e5a82c6d82014a2399eb2af4bb77
lihui.liu <lihui.liu@surgnova.com>
2022-01-16 15:08:45 +0800

    add battery

--- battery_analysis.py
+++ battery_analysis.py
@@ -1,7 +1,7 @@
-import numpy as np
+ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#%matplotlib auto
+%matplotlib auto
 #%matplotlib inline
 from matplotlib.animation import FuncAnimation
 from pandas.core.indexes import interval
